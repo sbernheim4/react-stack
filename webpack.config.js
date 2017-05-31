c0b574239df49dc069b8758c33ac1e81b4afb51e
Samuel Bernheim <sambernheim@gmail.com>
2017-05-31 16:25:59 -0400

    Get something working

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	entry: __dirname + '/src/index.js',
+	output: {
+		path: __dirname + '/public',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}
+		]
+	},
+	resolve: {
+		extensions: ['*', '.js', '.json']
+	}
+};
